0a642ffc4c5b0931bee97f7d8cf5c8c5a2f6bd08
Hayden Beck <haydenabeck@gmail.com>
2021-06-06 22:43:13 -0500

    added header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Footer() {
-  return <></>;
+  return (
+    <>
+      <div class="social-icons">
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/hayden-beck-17109419a/">
+              <i class="fa fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/haydenabeck">
+              <i class="fa fa-github"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,52 @@
 import React from "react";
 
 function Header() {
-  return <></>;
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Hello, I'm Hayden
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Projects
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
