4a478bd8bc0b3e66b712a3d06c1561ec8fbd7c92
Hayden Beck <haydenabeck@gmail.com>
2021-06-07 16:49:41 -0500

    building

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer.js";
 import Header from "./components/Header.js";
 import Projects from "./components/Projects.js";
+import Homepage from "./components/Homepage.js";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Projects />
-      <Footer />
-      {/* OTHER WHOLE COMPONENTS */}
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route exact path={"/"}>
+            <Homepage />
+          </Route>
+          <Route exact path={["/about"]}>
+            <About />
+          </Route>
+          <Route exact path={["/contact"]}>
+            <Contact />
+          </Route>
+          <Route exact path={["/projects"]}>
+            <Projects />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
